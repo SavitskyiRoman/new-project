24417377f208e74b05b6b30c82c42e5f678a0531
Oleksandr Reshetnikov <aleksandr.reshetnikov@gmail.com>
2016-07-25 15:28:23 +0300

    add(all): start new project

---
+++ js/function.js
@@ -0,0 +1,15 @@
+var searchDate = new Pikaday(
+    {
+        field: document.getElementById('searchDate'),
+        format: 'DD-MM-YYYY'
+    });
+var dueDate = new Pikaday(
+    {
+        field: document.getElementById('dueDate'),
+        format: 'DD-MM-YYYY'
+    });
+var createDate = new Pikaday(
+    {
+        field: document.getElementById('createDate'),
+        format: 'DD-MM-YYYY'
+    });

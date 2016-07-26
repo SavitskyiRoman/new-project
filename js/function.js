c0915da78e67a7ac794cb12dd5f1c5a5acdedc8b
Oleksandr Reshetnikov <aleksandr.reshetnikov@gmail.com>
2016-07-26 11:54:21 +0300

    fix(datapicker): add range between dueDate-startDate, customize range

--- js/function.js
+++ js/function.js
@@ -3,13 +3,60 @@ var searchDate = new Pikaday(
         field: document.getElementById('searchDate'),
         format: 'DD-MM-YYYY'
     });
-var dueDate = new Pikaday(
+/*var dueDate = new Pikaday(
     {
         field: document.getElementById('dueDate'),
-        format: 'DD-MM-YYYY'
+        format: 'DD-MM-YYYY',
+        minDate: new Date()
     });
 var createDate = new Pikaday(
     {
         field: document.getElementById('createDate'),
-        format: 'DD-MM-YYYY'
-    });
+        format: 'DD-MM-YYYY',
+        minDate: new Date()
+    });*/
+
+
+
+    var startDate,
+        endDate,
+        updateStartDate = function() {
+            createDate.setStartRange(startDate);
+            dueDate.setStartRange(startDate);
+            dueDate.setMinDate(startDate);
+        },
+        updateEndDate = function() {
+            createDate.setEndRange(endDate);
+            createDate.setMaxDate(endDate);
+            dueDate.setEndRange(endDate);
+        },
+        createDate = new Pikaday({
+            field: document.getElementById('createDate'),
+            minDate: new Date(),
+            maxDate: new Date(2020, 12, 31),
+            onSelect: function() {
+                startDate = this.getDate();
+                updateStartDate();
+            }
+        }),
+        dueDate = new Pikaday({
+            field: document.getElementById('dueDate'),
+            minDate: new Date(),
+            maxDate: new Date(2020, 12, 31),
+            onSelect: function() {
+                endDate = this.getDate();
+                updateEndDate();
+            }
+        }),
+        _startDate = createDate.getDate(),
+        _endDate = dueDate.getDate();
+
+        if (_startDate) {
+            startDate = _startDate;
+            updateStartDate();
+        }
+
+        if (_endDate) {
+            endDate = _endDate;
+            updateEndDate();
+        }
